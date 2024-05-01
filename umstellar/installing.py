c49519ff2dc9a7c06d23f75b6a1c301d6634347c
madomado <madonuko@outlook.com>
2024-05-01 18:03:04 +0800

    feat: progress bar untested because kill me

--- umstellar/installing.py
+++ umstellar/installing.py
@@ -1,3 +1,10 @@
+# WARN: horrible code incoming
+# This piece of progress bar + parallel install thingy is made by mado in something like a day.
+# No time has been wasted on things like refactoring stuff. I don't care, and I can't be bothered.
+# Don't blame me for the abusive use of global variables, poor logics and lack of error handling.
+# I have no idea why everyone else in my team is a deadline fighter and no one has the intention
+# to even attempt to meet deadlines.
+import logging
 import multiprocessing as mp
 import os
 import sys
@@ -5,7 +12,10 @@ import time
 from contextlib import suppress
 from multiprocessing import Process, Queue
 from subprocess import PIPE, Popen
+from threading import Thread
 from typing import IO, Callable, Iterable
+
+from gi.repository import GLib, Gtk
 
 from . import App, Dnf, DnfRm, Flatpak, Payload, Procedure
 
@@ -16,26 +26,84 @@ PROC = 1
 SCRIPT = 2
 DNFRM = 3
 DNFIN = 4
+DNFDL = 6
 FLATPAK = 5
+before_dnf = 0
+dnf_total = 0
+
+
+class InstallProgressWindow(Gtk.ApplicationWindow):
+    lbl = Gtk.Label(label="Test Label")
+    progress = Gtk.ProgressBar(hexpand=True)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.set_default_size(800, 600)
+        # self.header_bar = Adw.HeaderBar()
+        # self.header_bar.set_show_end_title_buttons(False)
+        self.box = Gtk.Box(
+            orientation=Gtk.Orientation.VERTICAL,
+            vexpand=False,
+            hexpand=True,
+            margin_top=25,
+            margin_end=25,
+            margin_start=25,
+            margin_bottom=25,
+        )
+        self.box.append(self.lbl)
+        self.box.append(self.progress)
+        self.set_child(self.box)
+        self.th = Thread(target=self.th_update_progress)
+        self.th.daemon = True
+        self.th.start()
+
+    def th_update_progress(self):
+        while job.value != total_jobs:
+            GLib.idle_add(self.update_progress, job.value)
+            time.sleep(0.1)
+
+    def update_progress(self, value: float):
+        self.progress.set_fraction(value)
+        self.lbl.set_text(self.determine_state())
+        return False
+
+    def determine_state(self) -> str:
+        global state, job, total_jobs, PROC, SCRIPT, DNFRM, DNFIN, DNFDL, FLATPAK
+        match state.value:
+            case PROC:
+                return f"[{job.value}/{total_jobs}] Running function"
+            case SCRIPT:
+                return f"[{job.value}/{total_jobs}] Running script"
+            case DNFRM:
+                return f"[{job.value}/{total_jobs}] Removing packages"
+            case DNFIN:
+                return f"[{job.value}/{total_jobs}] Installing RPM packages"
+            case DNFDL:
+                return f"[{job.value}/{total_jobs}] Downloading RPM packages"
+            case FLATPAK:
+                return f"[{job.value}/{total_jobs}] Installing Flatpak packages"
+            case _:
+                logging.warn(f"Unknown state.value: {state.value}")
 
 
 def gather[T: Payload](cls: type[T], apps: dict[str, App]) -> list[T]:
-    return list(
+    return [
         payload
         for app in apps.values()
         for payload in app.payloads
         if isinstance(payload, cls)
-    )
+    ]
 
 
 def process_installs(apps: dict[str, App]):
     global total_jobs
-    # FIXME: add dnf downloads and installs separately
-    total_jobs = len(apps)
+    total_jobs = len(apps) + len(gather(Dnf, apps))
     p = mp.Process(target=install, args=(apps))
     p.start()
-    # TODO: start the GUI here
+    gui = InstallProgressWindow()
+    gui.present()
     p.join()
+    gui.th.join()
 
 
 def install(apps: dict[str, App]):
@@ -43,48 +111,48 @@ def install(apps: dict[str, App]):
     dnfrms = gather(DnfRm, apps)
     dnfs = gather(Dnf, apps)
     flatpaks = gather(Flatpak, apps)
-    pre = [
-        payload
-        for app in apps.values()
-        for payload in app.payloads
-        if payload.priority < 0
-    ]
-    pre.sort(key=lambda p: p.priority)
-    for p in pre:
-        state.value = PROC if isinstance(p, Procedure) else SCRIPT
-        job.value += 1
-        p()
+    run_special_payloads(apps, lambda payload: payload.priority < 0)
+    # WARN: dnf/5 is a giant mess and it will fail
+    # somehow someone here please do some proper error handling or sth
+    # I don't really know like… how exactly we should do it, but for now
+    # I'll just pray that they are just edge cases…
+    # -- mado
     state.value = DNFRM
     run_dnf("rm", map(lambda x: x.name, dnfrms))
-    state.value = DNFIN
+    state.value = 0
     run_dnf("in", map(lambda x: x.name, dnfs))
     state.value = FLATPAK
     run_flatpak(map(lambda x: x.name, flatpaks))
-    post = [
-        payload
-        for app in apps.values()
-        for payload in app.payloads
-        if payload.priority > 0
-    ]
-    post.sort(key=lambda p: p.priority)
-    for p in post:
+    job.value += len(flatpaks)
+    run_special_payloads(apps, lambda payload: payload.priority > 0)
+
+
+def run_special_payloads(apps: dict[str, App], filter: Callable[[Payload], bool]):
+    payloads = [p for app in apps.values() for p in app.payloads if filter(p)]
+    payloads.sort(key=lambda p: p.priority)
+    for p in payloads:
         state.value = PROC if isinstance(p, Procedure) else SCRIPT
         job.value += 1
         p()
 
 
 def run_dnf(act: str, pkgs: Iterable[str]):
+    global before_dnf
     # TODO: separate install and download parsing
-    run_with_line_parse(["sudo", "dnf5", act, *pkgs], line_parse=_dnf5_line_parse)
+    run_with_line_parse(["sudo", "dnf5", act, "-y", *pkgs], line_parse=_dnf5_line_parse)
 
 
 def run_flatpak(pkgs: Iterable[str]):
     # TODO: implement progress tracking for flatpak
-    run_with_line_parse(["sudo", "flatpak", "install", *pkgs], line_parse=lambda _: ())
+    # NOTE: I don't think it's actually possible…?
+    run_with_line_parse(
+        ["sudo", "flatpak", "install", "--noninteractive", *pkgs],
+        line_parse=lambda _: (),
+    )
 
 
 def _dnf5_line_parse(line: str):
-    global job
+    global job, before_dnf, dnf_total
     if not line.startswith("["):
         return
     right = line.find("]")
@@ -92,7 +160,17 @@ def _dnf5_line_parse(line: str):
     if right == -1 or mid == -1:
         return
     with suppress(ValueError):
-        job.value = int(line[1:mid])
+        if dnf_total != int(line[mid + 1 : right]):
+            # downloading -> installing
+            before_dnf = job.value
+            if state.value == 0:
+                state.value = DNFDL
+            elif state.value == DNFDL:
+                state.value = DNFIN
+            else:
+                # FIXME: I don't know
+                pass
+        job.value = before_dnf + int(line[1:mid])
 
 
 def _just_read(qin: Queue, qres: Queue, fd: IO[bytes]):
@@ -108,6 +186,7 @@ def _just_read(qin: Queue, qres: Queue, fd: IO[bytes]):
     qin.get()
 
 
+# Copied from terrapkg/mkproj
 def run_with_line_parse(
     cmd: list[str], prefix: str = "┃ ", *, line_parse: Callable[[str]]
 ) -> tuple[int, str, str]:
